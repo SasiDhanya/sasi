e3750739cec98dcf6b3ca2662b1dd46cd1b39b89
SasiDhanya <sasi.patra05@gmail.com>
2024-08-10 00:31:28 +0530

    h
    h

---
+++ Example.java
@@ -0,0 +1,17 @@
+package sasi;
+
+import java.awt.Frame;
+
+public class Example {
+// MY First Attemp
+	public static void main(String[] args) {
+		System.out.println("Hello my first Maven Project");
+ 
+		Frame ib=new Frame("Hai");
+		ib.setVisible(true);
+		ib.setLayout(null);
+		ib.setSize(100,100);
+		
+	}
+
+}
